08b82ca428e2eb83b119a765d9423e846d0db6a6
Abylai <aba.omar@mail.ru>
2022-06-02 18:07:58 +0600

    Task063

---
+++ Task063/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 63: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от 1 до N.
+// N = 5 -> "1, 2, 3, 4, 5"
+// N = 6 -> "1, 2, 3, 4, 5, 6"
+
+
+Console.WriteLine("Задайте число n: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int result = Rekurs(n);
+Console.WriteLine(result);
+
+int Rekurs(int number)
+{
+    if (number == 1)
+        return 1;
+    int temp = (Rekurs(number - 1));
+    Console.Write(temp + ", " );
+    return number;
+}
